caacb869d311898bdb08cf0a2e94e663006f551e
ianjuma <ian.juma@yahoo.com>
2014-05-05 21:39:28 +0300

    model -> user class :skull:

--- restAPI/models.py
+++ restAPI/models.py
@@ -11,15 +11,15 @@ engine = create_engine('mysql://root:root@localhost/blog', pool_recycle=3600)
 
 
 users = Table('contact', metadata,
-  Column('email', String(50), primary_key=True),
-  Column('name', String(50)),
-  Column('message' String(50)),
-)
+              Column('email', String(50), primary_key=True),
+              Column('name', String(50)),
+              Column('message', String(50))
+              )
 
 
 class User(Base):
-	__tablename__ = 'contact'
+    __tablename__ = 'contact'
 
-	email = Column(String(50), primary_key=True)
-	name = Column(String(50))
-	message = Column(String(500))
+    email = Column(String(50), primary_key=True)
+    name = Column(String(50))
+    message = Column(String(500))
