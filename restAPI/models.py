e2cafe30885360bc287da54b50e06d0f3c0ffeab
ianjuma <ian.juma@yahoo.com>
2014-05-05 21:56:41 +0300

    metadata models

--- restAPI/models.py
+++ restAPI/models.py
@@ -4,6 +4,7 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy import Table
+from sqlalchemy import *
 
 Base = declarative_base()
 
