3038ad826a73e5684681e1fada160a7b78d0231b
ianjuma <ian.juma@yahoo.com>
2014-05-05 21:26:45 +0300

    models fix

--- restAPI/models.py
+++ restAPI/models.py
@@ -3,10 +3,19 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
+from sqlalchemy import Table
 
 Base = declarative_base()
 
 engine = create_engine('mysql://root:root@localhost/blog', pool_recycle=3600)
+
+
+users = Table('contact', metadata,
+  Column('email', String(50), primary_key=True),
+  Column('name', String(50)),
+  Column('message' String(50)),
+)
+
 
 class User(Base):
 	__tablename__ = 'contact'
