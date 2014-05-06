2e49c1b6f1991168f456e07944b5926b820ad6cc
ianjuma <ian.juma@yahoo.com>
2014-05-06 20:53:47 +0300

    model update :skull:

---
+++ restAPI/static/js/model.app.js
@@ -0,0 +1,50 @@
+var app = {};
+
+app.Contact = Backbone.Model.extend({
+
+	urlRoot: "/api/v1/contact/",
+
+	initialize: function() {
+		console.log('contact model has been initialized');
+
+		this.on("change", function() {
+			console.log("Attributes in this Model have changed");
+			console.log(this.toJSON());
+		});
+	},
+
+	defaults: {
+      name: '',
+      email: '',
+      message: '',
+    }
+
+	setName: function(thename) {
+		this.set({name: thename});
+	},
+
+	setEmail: function(email) {
+		this.set({email: email});
+	},
+
+	setMessage: function(message) {
+		this.set({message: message});
+	},
+
+	setInterval(function() {
+  		this.fetch();
+	}, 10000);
+
+});
+
+
+var name = $('#contact-name').val();
+var email = $('#contact-mail').val();
+var message = $('#contact-message').val();
+
+$('#contact-send').click(function() {
+	var app.contact = new app.Contact();
+
+	app.contact.set({ "name": name, "email": email, "message": message });
+	app.contact.save();
+});
